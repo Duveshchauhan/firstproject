c2795224d2a4073d83e05bbdbd419e6977401a1f
Chauhan Duvesh (SX/ETD13-MM) <dux2kor@bosch.com>
2024-08-05 16:39:47 +0530

    another repo

--- README.md
+++
@@ -1,2 +0,0 @@
-# firstproject
-second commit try
--- g.java
+++
@@ -1,8 +0,0 @@
-public class FirstJava
-    {
-      public static void main(String[] args)
-       {
-         System.out.pritnln("Hello! Welcome to Jenkins!") ;
-       }
-    }
-
